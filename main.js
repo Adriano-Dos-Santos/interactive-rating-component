5f26bf15b671d3d360abaa8273e52d6a84d0c7a0
Adriano Dos Santos <adsc999@gmail.com>
2023-02-02 20:52:20 -0300

    form widget styles complete, font-size fixed

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-document.getElementById('btn').addEventListener("click", function () {
+document.getElementById("btn").addEventListener("click", function () {
   let score = document.querySelector('input[name="score"]:checked');
- if (score) {
-   document.getElementById("selected-value").innerHTML = "selected Value: " + score.value;
-   document.getElementById("rating").style.display = "none";
-   document.getElementById("thanks").style.display = "block";
- }
-})
+  if (score) {
+    document.getElementById("selected-value").innerHTML =
+      "selected Value: " + score.value;
+    document.getElementById("rating").style.display = "none";
+    document.getElementById("thanks").style.display = "block";
+  }
+});
