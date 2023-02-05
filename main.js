6f5ca9b2b6ffdde2bb44a0b4c60d400302184cb1
Adriano Dos Santos <adsc999@gmail.com>
2023-02-05 07:53:16 -0300

    fixed some error and added missing details

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.getElementById("btn").addEventListener("click", function () {
   let score = document.querySelector('input[name="score"]:checked');
   if (score) {
     document.getElementById("selected-value").innerHTML =
-      "You selected Value: " + score.value + " out of 5";
+      "You selected " + score.value + " out of 5";
     document.getElementById("rating").style.display = "none";
     document.getElementById("thanks").style.display = "block";
   }
